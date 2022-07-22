d13bf1ad252d0cbfc3d41b1aeca3870e1e657a9f
Chayan <chayanforyou007@gmail.com>
2022-07-22 23:22:27 +0600

    modify project structure

--- app/src/main/java/com/example/raytemp/MainActivity.java
+++ app/src/main/java/com/example/raytemp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.raytemp;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.widget.Toast;
+import com.example.raytemp.databinding.ActivityMainBinding;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import uk.co.etiltd.thermalib.ThermaLib;
 
@@ -16,19 +31,37 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    public static int REQUEST_LOCATION = 1001;
+    public static int REQUEST_PERMISSIONS = 1001;
 
-    private static final String[] PERMISSIONS_LOCATION = {
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION};
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // permission ACCESS_COARSE_LOCATION
-        checkPermission();
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_device)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        // Handle run time permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (getAllPermission()) initializeThermaLib();
+        } else {
+            initializeThermaLib();
+        }
     }
 
     private void initializeThermaLib() {
@@ -39,26 +72,84 @@ public class MainActivity extends AppCompatActivity {
         therm.startScanForDevices(ThermaLib.Transport.BLUETOOTH_LE, 5); // TransportType, TimeoutInSeconds
     }
 
-    // Function to check and request permission.
-    public void checkPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // Requesting the permission
-            ActivityCompat.requestPermissions(this, PERMISSIONS_LOCATION, REQUEST_LOCATION);
-        } else {
-            initializeThermaLib();
+    private boolean getAllPermission() {
+
+        List<String> list = new ArrayList<>();
+
+        int fineLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int coarseLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (fineLocation != PackageManager.PERMISSION_GRANTED) {
+            list.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
+
+        if (coarseLocation != PackageManager.PERMISSION_GRANTED) {
+            list.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            int bluetoothScan = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN);
+            int bluetoothConnect = ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT);
+
+            if (bluetoothScan != PackageManager.PERMISSION_GRANTED) {
+                list.add(Manifest.permission.BLUETOOTH_SCAN);
+            }
+
+            if (bluetoothConnect != PackageManager.PERMISSION_GRANTED) {
+                list.add(Manifest.permission.BLUETOOTH_CONNECT);
+            }
+        }
+
+        if (!list.isEmpty()) {
+            final int size = list.size();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(list.toArray(new String[size]), REQUEST_PERMISSIONS);
+                return false;
+            }
+        }
+        return true;
     }
 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_LOCATION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+        if (requestCode == REQUEST_PERMISSIONS) {
+            // Check if all permissions are granted
+            boolean allGranted = false;
+            for (int grantResult : grantResults) {
+                if (grantResult == PackageManager.PERMISSION_GRANTED) {
+                    allGranted = true;
+                } else {
+                    allGranted = false;
+                    break;
+                }
+            }
+
+            if (allGranted) {
                 initializeThermaLib();
             } else {
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+                builder.setTitle("Need Multiple Permissions");
+                builder.setMessage("This app needs Bluetooth & Location permission");
+                builder.setPositiveButton("Grant", (dialog, which) -> {
+                    dialog.cancel();
+                    getAllPermission();
+                });
+                builder.setNegativeButton("Cancel", (dialog, which) -> {
+                    dialog.cancel();
+                    finish();
+                });
+                builder.show();
             }
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
