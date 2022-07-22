090440364b2b357d2edb5fc4ba9cbbde848a7f96
MD Al Mamun (IT) <mdmamun@ekfc.ae>
2022-07-23 00:00:02 +0600

    remove unused import

--- app/src/main/java/com/example/raytemp/MainActivity.java
+++ app/src/main/java/com/example/raytemp/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.raytemp;
 
 import android.Manifest;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
